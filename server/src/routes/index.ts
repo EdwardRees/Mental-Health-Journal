0e87bdfe6c494562735a49c82909ea389f8178cc
Edward Rees <erees.hk@gmail.com>
2022-04-19 22:19:56 -0700

    route comment setup

---
+++ server/src/routes/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Routes for the server
+ */
+// import { Router } from "express";
+
+/**
+ * Routes for authentication
+ */
+// import authRoutes from './auth';
+
+/**
+ * Routes for user profile
+ */
+//import userProfileRoutes from './user-profile';
+
+/**
+ * Routes for gratitude journal
+ */
+//import gratitudeJournalRoutes from './gratitude-journal';
+
+/**
+ * Routes for affirmation
+ */
+// import affirmationRoutes from './affirmation';
+
+/**
+ * Routes for mood journal
+ */
+// import moodJournalRoutes from './mood-journal';
