29952ff7f99d4cce70c4feda3749c7a282195e2a
Edward Rees <erees.hk@gmail.com>
2022-05-12 20:07:44 -0700

    Updated migration for scj

--- self-care-journal/server/prisma/migrations/20220513030717_test/migration.sql
+++ self-care-journal/server/prisma/migrations/20220513030717_test/migration.sql
@@ -1,3 +1,15 @@
+-- CreateEnum
+CREATE TYPE "RepeatType" AS ENUM ('ONCE', 'EVERY');
+
+-- CreateEnum
+CREATE TYPE "RepeatCycle" AS ENUM ('DAILY', 'WEEKLY', 'MONTHLY');
+
+-- CreateEnum
+CREATE TYPE "RepeatEnd" AS ENUM ('NEVER', 'ON', 'AFTER');
+
+-- CreateEnum
+CREATE TYPE "Weekday" AS ENUM ('MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY');
+
 -- CreateTable
 CREATE TABLE "Auth" (
     "id" SERIAL NOT NULL,
@@ -61,6 +73,38 @@ CREATE TABLE "MoodEntry" (
 );
 
 -- CreateTable
+CREATE TABLE "SelfCareActivity" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "activity" TEXT NOT NULL,
+    "repeatType" "RepeatType" NOT NULL DEFAULT E'ONCE',
+    "repeatCycle" "RepeatCycle",
+    "dailyRepeatTimes" INTEGER[],
+    "weeklyRepeatDays" "Weekday"[],
+    "monthlyRepeatDays" INTEGER[],
+    "repeatEvery" INTEGER,
+    "repeatEnd" "RepeatEnd",
+    "repeatAfter" INTEGER,
+    "active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "SelfCareActivity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CompletedSelfCareActivity" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "selfCareActivityId" INTEGER NOT NULL,
+    "completedOn" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "CompletedSelfCareActivity_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "PasswordReset" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -106,6 +150,15 @@ ALTER TABLE "AffirmationEntry" ADD CONSTRAINT "AffirmationEntry_userId_fkey" FOR
 
 -- AddForeignKey
 ALTER TABLE "MoodEntry" ADD CONSTRAINT "MoodEntry_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "SelfCareActivity" ADD CONSTRAINT "SelfCareActivity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CompletedSelfCareActivity" ADD CONSTRAINT "CompletedSelfCareActivity_selfCareActivityId_fkey" FOREIGN KEY ("selfCareActivityId") REFERENCES "SelfCareActivity"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CompletedSelfCareActivity" ADD CONSTRAINT "CompletedSelfCareActivity_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "PasswordReset" ADD CONSTRAINT "PasswordReset_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
